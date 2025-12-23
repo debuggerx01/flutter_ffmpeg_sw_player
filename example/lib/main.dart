40e55a7e60e1554237461c53e1ab7b128cc853dd
DebuggerX <dx8917312@163.com>
2025-12-23 17:43:31 +0800

    降低SDK依赖版本

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -16,9 +16,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: .fromSeed(
+        colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.deepPurple,
-          brightness: .dark,
+          brightness: Brightness.dark,
         ),
       ),
       home: const HomePage(),
